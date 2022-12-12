6f7c4135764d0c45eabdbca3672e13bb0358a861
jeanfer7 <jeanfersellan@outlook.com>
2022-12-12 11:29:45 +0100

    Fin cambio de imagen

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO vets VALUES (default, 'James', 'Carter');
-INSERT INTO vets VALUES (default, 'Helen', 'Leary');
-INSERT INTO vets VALUES (default, 'Linda', 'Douglas');
+INSERT INTO vets VALUES (default, 'Javier', 'Rodriguez');
+INSERT INTO vets VALUES (default, 'Helena', 'Ruiz');
+INSERT INTO vets VALUES (default, 'Luis', 'Martinez');
 INSERT INTO vets VALUES (default, 'Rafael', 'Ortega');
-INSERT INTO vets VALUES (default, 'Henry', 'Stevens');
-INSERT INTO vets VALUES (default, 'Sharon', 'Jenkins');
+INSERT INTO vets VALUES (default, 'Enrique', 'García');
+INSERT INTO vets VALUES (default, 'Sharon', 'García');
 
-INSERT INTO specialties VALUES (default, 'radiology');
-INSERT INTO specialties VALUES (default, 'surgery');
-INSERT INTO specialties VALUES (default, 'dentistry');
+INSERT INTO specialties VALUES (default, 'Radiologo');
+INSERT INTO specialties VALUES (default, 'Cirujano');
+INSERT INTO specialties VALUES (default, 'Dentista');
 
 INSERT INTO vet_specialties VALUES (2, 1);
 INSERT INTO vet_specialties VALUES (3, 2);
@@ -15,23 +15,23 @@ INSERT INTO vet_specialties VALUES (3, 3);
 INSERT INTO vet_specialties VALUES (4, 2);
 INSERT INTO vet_specialties VALUES (5, 1);
 
-INSERT INTO types VALUES (default, 'cat');
-INSERT INTO types VALUES (default, 'dog');
-INSERT INTO types VALUES (default, 'lizard');
-INSERT INTO types VALUES (default, 'snake');
-INSERT INTO types VALUES (default, 'bird');
-INSERT INTO types VALUES (default, 'hamster');
+INSERT INTO types VALUES (default, 'Gato');
+INSERT INTO types VALUES (default, 'Perro');
+INSERT INTO types VALUES (default, 'Lagartija');
+INSERT INTO types VALUES (default, 'Serpiente');
+INSERT INTO types VALUES (default, 'Pajaro');
+INSERT INTO types VALUES (default, 'Hamster');
 
-INSERT INTO owners VALUES (default, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
-INSERT INTO owners VALUES (default, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
-INSERT INTO owners VALUES (default, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland', '6085558763');
-INSERT INTO owners VALUES (default, 'Harold', 'Davis', '563 Friendly St.', 'Windsor', '6085553198');
-INSERT INTO owners VALUES (default, 'Peter', 'McTavish', '2387 S. Fair Way', 'Madison', '6085552765');
-INSERT INTO owners VALUES (default, 'Jean', 'Coleman', '105 N. Lake St.', 'Monona', '6085552654');
-INSERT INTO owners VALUES (default, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085555387');
-INSERT INTO owners VALUES (default, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683');
-INSERT INTO owners VALUES (default, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435');
-INSERT INTO owners VALUES (default, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487');
+INSERT INTO owners VALUES (default, 'Juan', 'Ruiz', 'Calle Josefa Valcarcel', 'Madrid', '6085551023');
+INSERT INTO owners VALUES (default, 'Beatriz', 'Cumplido', 'Calle Josefa Valcarcel', 'Madrid', '6085551749');
+INSERT INTO owners VALUES (default, 'Eduardo', 'Rodriguez', 'Calle Josefa Valcarcel', 'Madrid', '6085558763');
+INSERT INTO owners VALUES (default, 'Diego', 'Velasco', 'Calle Josefa Valcarcel', 'Madrid', '6085553198');
+INSERT INTO owners VALUES (default, 'Peter', 'Consuegra', 'Calle Josefa Valcarcel', 'Madrid', '6085552765');
+INSERT INTO owners VALUES (default, 'Jeronimo', 'Martinez', 'Calle Josefa Valcarcel', 'Madrid', '6085552654');
+INSERT INTO owners VALUES (default, 'Jeff', 'Blazquez', 'Calle Paterna', 'Valencia', '6085555387');
+INSERT INTO owners VALUES (default, 'Maria', 'Escobito', 'Calle Paterna', 'Valencia', '6085557683');
+INSERT INTO owners VALUES (default, 'David', 'Sevillar', 'Calle Paterna', 'Valencia', '6085559435');
+INSERT INTO owners VALUES (default, 'Carlos', 'Lopez', 'Calle Paterna', 'Valencia', '6085555487');
 
 INSERT INTO pets VALUES (default, 'Leo', '2010-09-07', 1, 1);
 INSERT INTO pets VALUES (default, 'Basil', '2012-08-06', 6, 2);
@@ -47,7 +47,7 @@ INSERT INTO pets VALUES (default, 'Freddy', '2010-03-09', 5, 9);
 INSERT INTO pets VALUES (default, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets VALUES (default, 'Sly', '2012-06-08', 1, 10);
 
-INSERT INTO visits VALUES (default, 7, '2013-01-01', 'rabies shot');
-INSERT INTO visits VALUES (default, 8, '2013-01-02', 'rabies shot');
-INSERT INTO visits VALUES (default, 8, '2013-01-03', 'neutered');
-INSERT INTO visits VALUES (default, 7, '2013-01-04', 'spayed');
+INSERT INTO visits VALUES (default, 7, '2013-01-01', 'Vacuna de la rabia');
+INSERT INTO visits VALUES (default, 8, '2013-01-02', 'Vacuna de la rabia');
+INSERT INTO visits VALUES (default, 8, '2013-01-03', 'Revisión');
+INSERT INTO visits VALUES (default, 7, '2013-01-04', 'Vacuna');
